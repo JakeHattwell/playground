019989f4e062c10529be2da728c15fb9ca87ccb2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-31 05:21:10 +0000

    Deploy JakeHattwell/wormjam-ci-test to github.com/JakeHattwell/wormjam-ci-test.git:gh-pages

--- push.sh
+++ push.sh
@@ -4,14 +4,14 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
+  git checkout -b devel
   git add . *.html
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/JakeHattwell/wormjam-ci-test.git > /dev/null 2>&1
-  git push --quiet --set-upstream devel devel 
+  git remote add origin-devel https://${GH_TOKEN}@github.com/JakeHattwell/wormjam-ci-test.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-devel devel 
 }
 
 setup_git
