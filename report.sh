52a5f66c3b9781e49f9d414d34dc79ce0f36a601
Jake Hattwell <j.hattwell@uq.edu.au>
2019-11-04 13:28:51 +1000

    git push

--- report.sh
+++ report.sh
@@ -1,1 +1,1 @@
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST --data-urlencode "{\"body\": \"$(cat results.txt)\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST --data-urlencode "{\"body\": \"Hello World\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
