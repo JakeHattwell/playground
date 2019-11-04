1a8715afad4ef515f45208282aa201d9ac0d0c3c
Jake Hattwell <j.hattwell@uq.edu.au>
2019-11-04 14:15:47 +1000

    Try again

--- report.sh
+++ report.sh
@@ -1,1 +1,1 @@
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST --data-urlencode "{\"body\": \"Hello World\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST --d "{\"body\": \"Hello World\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
