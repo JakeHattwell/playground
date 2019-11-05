f67270e7e1251cbd5ee146d86af72d6ce02c8fdf
Jake Hattwell <j.hattwell@uq.edu.au>
2019-11-05 13:54:33 +1000

    Trying a different method

--- report.sh
+++ report.sh
@@ -1,2 +1,2 @@
-COMMENT="Hi There"
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\":\"Checking Commenting Capabilities\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+COMMENT='{"body":"Checking Commenting Capabilities"}'
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d $COMMENT "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
