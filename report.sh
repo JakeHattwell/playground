4cacb84f60f414141047c5542c8c89cdbef64e67
Jake Hattwell <j.hattwell@uq.edu.au>
2019-11-05 10:51:15 +1000

    Trying again

--- report.sh
+++ report.sh
@@ -1,2 +1,2 @@
 COMMENT="Hi There"
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d '{"body": "$COMMENT"}' "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{'body': $COMMENT}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
