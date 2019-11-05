a906a23aafd6aff6b0d6d6254f38ffc7b85e409a
Jake Hattwell <j.hattwell@uq.edu.au>
2019-11-05 15:07:32 +1000

    Try again

--- report.sh
+++ report.sh
@@ -1,2 +1,2 @@
-COMMENT='{"body":"Checking Commenting Capabilities"}'
+COMMENT="{\"body\":\"Checking Commenting Capabilities\"}"
 curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d $COMMENT "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
