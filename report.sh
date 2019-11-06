43ca77c4734531a4deb8b0ef48048d7b29148b0f
Jake Hattwell <j.hattwell@uq.edu.au>
2019-11-06 14:55:45 +1000

    Trying again, removed shell script and added options to memote run

--- report.sh
+++
@@ -1,2 +0,0 @@
-COMMENT='{"body":"Checking Commenting Capabilities"}'
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d ${COMMENT} "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
