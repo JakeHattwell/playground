ca7a7cef99b13cdaaf70b4df3a33da268378da00
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-27 00:31:46 +0000

    Deploy JakeHattwell/wormjam-ci-test to github.com/JakeHattwell/wormjam-ci-test.git:gh-pages

--- python/manage_reports.py
+++ python/manage_reports.py
@@ -1,11 +1,6 @@
 import os
 import shutil
 import datetime
-
-if "index.html" in os.listdir():
-    shutil.copy("index.html","history/"+str(datetime.datetime.now())+".html")
-    print("File copied!")
-
 for i in os.listdir():
     if "travis_wait" in i:
         os.remove(i)
