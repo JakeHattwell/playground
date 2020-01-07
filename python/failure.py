bd35b505e2d9ab859e5bbf62034b2851c54afabd
Jake Hattwell <j.hattwell@uq.edu.au>
2020-01-07 14:02:41 +1000

    added failure check WITH DELIBERATE ERROR

--- python/failure.py
+++ python/failure.py
@@ -1,19 +1,15 @@
+import requests
 import json
 import sys
-import os
-import requests
 import datetime
+TRAVIS_BUILD_NUMBER = sys.argv[1]
+TRAVIS_BUILD_WEB_URL = sys.argv[2]
 
-DISCORD_ENDPOINT = sys.argv[1]
-TRAVIS_BUILD_NUMBER = sys.argv[2]
-TRAVIS_BUILD_WEB_URL = sys.argv[3]
-
-files = {'results.json': open('results.json', 'rb')}
 payload_json = {
     "embeds": [{
         "title": "WormJam CI Report",
-        "color": 16709211,
-        "description": "PR Merge",
+        "color": 10027008,
+        "description": "Travis CI Build",
         "fields":[
             {
                 "name": "Build Number",
@@ -27,8 +23,3 @@ payload_json = {
         "timestamp": str(datetime.datetime.now().isoformat())
     }]
 }
-
-r =requests.post(DISCORD_ENDPOINT,data=json.dumps(payload_json), headers={"Content-Type": "application/json"})
-r2 = requests.post(DISCORD_ENDPOINT, files=files)
-
-print(a+123)
